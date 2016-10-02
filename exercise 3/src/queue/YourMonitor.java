bab8bda671f10248bbedc0282d016b73e7a9f8b3
Anton Friberg <anton.friberg93@gmail.com>
2016-10-02 14:59:58 +0200

    Add implementation for exercise 3.

--- exercise 3/src/queue/YourMonitor.java
+++ exercise 3/src/queue/YourMonitor.java
@@ -2,11 +2,15 @@ package queue;
 
 class YourMonitor {
 	private int nCounters;
-	// Put your attributes here...
+	private int lastCustomerArrived = 99;   // Start with zero after inc.
+	private int lastCustomerServed = 99;    // - " -
+    private int nbrOfFreeClerks;
+    private int lca;                        // Last counter assigned.
+    private boolean[] counterFree;
 
 	YourMonitor(int n) { 
 		nCounters = n;
-		// Initialize your attributes here...
+		counterFree = new boolean[n];
 	}
 
 	/**
@@ -14,24 +18,37 @@ class YourMonitor {
 	 * There is never more than 100 customers waiting.
 	 */
 	synchronized int customerArrived() { 
-		// Implement this method...
-		return 0;
+		lastCustomerArrived = (lastCustomerArrived + 1) % 100;
+		notifyAll();
+        return lastCustomerArrived;
 	}
 
 	/**
 	 * Register the clerk at counter id as free. Send a customer if any. 
 	 */
 	synchronized void clerkFree(int id) { 
-		// Implement this method...
+		if (!counterFree[id]) { // Ignoring multiple button pushes.
+            nbrOfFreeClerks++;
+            counterFree[id] = true;
+            notifyAll();
+        }
 	}
 
 	/**
 	 * Wait for there to be a free clerk and a waiting customer, then
-	 * return the cueue number of next customer to serve and the counter 
+	 * return the queue number of next customer to serve and the counter
 	 * number of the engaged clerk.
 	 */
 	synchronized DispData getDisplayData() throws InterruptedException { 
-		// Implement this method...
-		return null;
+		while (lastCustomerArrived == lastCustomerServed || nbrOfFreeClerks == 0) {
+            wait();
+        }
+        while (!counterFree[lca]) lca = (lca + 1)%nCounters; // Fairness.
+        DispData ans = new DispData();
+        ans.counter = lca;
+        counterFree[lca] = false;
+        nbrOfFreeClerks--;
+        ans.ticket = lastCustomerServed = (lastCustomerServed + 1) % 100;
+        return ans;
 	}
 }
