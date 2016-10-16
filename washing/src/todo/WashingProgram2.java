697b1570b305bf75304d7ff0d48f7adf693f69f8
AntonFriberg <anton.friberg93@gmail.com>
2016-10-16 18:26:06 +0200

    Implement washing programs 0-3.

---
+++ washing/src/todo/WashingProgram2.java
@@ -0,0 +1,116 @@
+/*
+ * Real-time and concurrent programming course, laboratory 3
+ * Department of Computer Science, Lund Institute of Technology
+ *
+ * PP 980812 Created
+ * PP 990924 Revised
+ */
+
+package todo;
+
+import done.AbstractWashingMachine;
+
+/**
+ * Program 3 of washing machine. Does the following:
+ * <UL>
+ *   <LI>Switches off heating
+ *   <LI>Switches off spin
+ *   <LI>Pumps out water
+ *   <LI>Unlocks the hatch.
+ * </UL>
+ */
+class WashingProgram2 extends WashingProgram {
+	int preWashTime = 15*60*1000;
+	int washTime = 30*60*1000;
+	int rinseTime = 2*60*1000;
+	int centTime = 5*60*1000;
+
+	// ------------------------------------------------------------- CONSTRUCTOR
+
+	/**
+	 * @param   mach             The washing machine to control
+	 * @param   speed            Simulation speed
+	 * @param   tempController   The TemperatureController to use
+	 * @param   waterController  The WaterController to use
+	 * @param   spinController   The SpinController to use
+	 */
+	public WashingProgram2(AbstractWashingMachine mach,
+                           double speed,
+                           TemperatureController tempController,
+                           WaterController waterController,
+                           SpinController spinController) {
+		super(mach, speed, tempController, waterController, spinController);
+	}
+
+	// ---------------------------------------------------------- PUBLIC METHODS
+
+	/**
+	 * This method contains the actual code for the washing program. Executed
+	 * when the start() method is called.
+	 */
+	protected void wash() throws InterruptedException {
+
+		// Lock
+		myMachine.setLock(true);
+
+		// Fill
+		myWaterController.putEvent(new WaterEvent(this,
+				WaterEvent.WATER_FILL,
+				0.5));
+		mailbox.doFetch(); // Wait for Ack
+
+		// Heat water to 40C
+		myTempController.putEvent(new TemperatureEvent(this,
+				TemperatureEvent.TEMP_SET,
+				40));
+
+		// Switch on spin
+		mySpinController.putEvent(new SpinEvent(this,
+				SpinEvent.SPIN_SLOW));
+		Thread.sleep(preWashTime);
+
+        // Heat water to 90C
+        myTempController.putEvent(new TemperatureEvent(this,
+                TemperatureEvent.TEMP_SET,
+                90));
+        Thread.sleep(washTime);
+
+		// Turn off heat
+		myTempController.putEvent(new TemperatureEvent(this,
+				TemperatureEvent.TEMP_IDLE,
+				0));
+
+		// Rinse 5 times in cold water
+		for (int i = 0; i < 5; i++) {
+			//Drain
+			myWaterController.putEvent(new WaterEvent(this,
+					WaterEvent.WATER_DRAIN,
+					0.0));
+			mailbox.doFetch();
+
+			// Fill
+			myWaterController.putEvent(new WaterEvent(this,
+					WaterEvent.WATER_FILL,
+					0.5));
+			mailbox.doFetch(); // Wait for Ack
+			Thread.sleep(rinseTime);
+		}
+
+		// Drain
+		myWaterController.putEvent(new WaterEvent(this,
+				WaterEvent.WATER_DRAIN,
+				0.0));
+		mailbox.doFetch();
+
+		// Centrifuge
+		mySpinController.putEvent(new SpinEvent(this,
+				SpinEvent.SPIN_FAST));
+		Thread.sleep(centTime);
+
+		// Switch off spin
+		mySpinController.putEvent(new SpinEvent(this, SpinEvent.SPIN_OFF));
+
+		// Unlock
+        myMachine.setLock(false);
+	}
+}
